74e10f878bbe6bf024aa4c959f7a977d3cfc3e3b
olekwa <johnolekwa@gmail.com>
2023-03-16 20:53:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
- /**
-  * main - A program that print a line with puts function
-  * Return: 0 (success)
-  */
+#include <stdio.h>
+/**
+* main - A program that print a line with puts function
+* Return: 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
