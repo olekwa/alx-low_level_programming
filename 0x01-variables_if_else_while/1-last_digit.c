ab55d4efad51bae643171f8a647952abe4495508
olekwa <johnolekwa@gmail.com>
2023-03-20 08:52:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less 6 and not zero\n", n, m);
+		printf("Last digit of %d is %d and is less 6 and not 0\n", n, m);
 	}
 	return (0);
 }
