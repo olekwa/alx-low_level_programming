be555479bec4807bac684954431d7eb659dda44b
olekwa <johnolekwa@gmail.com>
2023-03-20 15:50:54 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - this is the main function
+  * Return: always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 100 || n == 113)
+		{
+			n++;
+			continue;
+		}
+	putchar(n);
+	n++;
+}
+putchar('\n');
+	return (0);
+}
