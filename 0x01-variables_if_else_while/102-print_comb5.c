6871a3ab47e799dcd304feb82562fa20e10176d9
olekwa <johnolekwa@gmail.com>
2023-03-20 19:55:14 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,24 +5,19 @@
  */
 int main(void)
 {
-	short a, b;
+	int a, b;
 
-	for (a = 0; b <= 100; a++)
+	for (a = 0; b < 100; a++)
 	{
-		for (b = 0; b <= 100; b++)
+		for (b = 0; b < 100; b++)
 		{
 			if (a < b)
 			{
-				short a1 = a / 10;
-				short a2 = a % 10;
-				short b1 = b / 10;
-				short b2 = b % 10;
-
-				putchar(a1 + '0');
-				putchar(a2 + '0');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
-				putchar(b1 + '0');
-				putchar(b2 + '0');
+				putchar((b / 10) +48);
+				putchar((b % 10) + 48);
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
