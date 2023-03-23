75028e7aa51ff4dc3e8aa460190a0f3343aff186
olekwa <johnolekwa@gmail.com>
2023-03-22 17:39:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet
- *
- * Return: 0
+ * print_alphabet_x10 - prints alphabet 10 x
+ * Return : 0
  */
 
-void print_alphabet(void)
-
+void print_alphabet_x10(void)
 {
 	char c;
+	int i = 0;
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (i <= 9)
 
 	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
 	_putchar(c);
+
 	}
 	_putchar('\n');
+	i++;
+	}
+
 }
