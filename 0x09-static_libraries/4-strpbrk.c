bebb67003ae4f203fb07d07985d3e2ae3e5df197
olekwa <johnolekwa@gmail.com>
2023-04-10 06:31:37 -0700

    update again

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -23,38 +23,3 @@ char *_strpbrk(char *s, char *accept)
 
 return ('\0');
 }
- 
-============================
- 
-5-strstr.c
-
-
-#include "main.h"
-/**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Always 0 (Success)
- */
-char *_strstr(char *haystack, char *needle)
-{
-	for (; *haystack != '\0'; haystack++)
-	{
-		char *l = haystack;
-		char *p = needle;
-
-
-		while (*l == *p && *p != '\0')
-		{
-			l++;
-			p++;
-		}
-
-
-		if (*p == '\0')
-			return (haystack);
-	}
-
-
-	return (0);
-}
