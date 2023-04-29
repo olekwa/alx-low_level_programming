66f1a6046b3fc4c059736b8f6b6259a191644d31
olekwa <johnolekwa@gmail.com>
2023-04-28 17:41:40 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *temp;
+list_t *gem;
 
 while (head)
 {
-temp = head->next;
+gem = head->next;
 free(head->str);
 free(head);
-head = temp;
+head = gem;
 }
 }
