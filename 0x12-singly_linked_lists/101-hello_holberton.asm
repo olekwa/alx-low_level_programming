56248e8f84d4f74b52af3e8ae5de8749d918491a
olekwa <johnolekwa@gmail.com>
2023-04-28 17:55:52 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
        global    main
           extern    printf
 main:
-	  mov   edi, format
+	  mov   edi, real
 	  xor   eax, eax
 	  call  printf
 	  mov 	eax, 0
 	  ret
-format: db `Hello, Holberton\n`,0
+real: db `Hello, Holberton\n`,0
